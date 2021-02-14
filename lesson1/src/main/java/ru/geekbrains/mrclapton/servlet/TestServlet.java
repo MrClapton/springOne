2d829a2211d8ed3e223449bf33efe5c44c0c90fc
MrClapton <f1nk121@yandex.ru>
2021-02-14 18:01:20 +0300

    Lesson-1

---
+++ lesson1/src/main/java/ru/geekbrains/mrclapton/servlet/TestServlet.java
@@ -0,0 +1,51 @@
+package ru.geekbrains.mrclapton.servlet;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.geekbrains.mrclapton.servlet.products.ProductService;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "TestServlet", urlPatterns = "/hello")
+public class TestServlet extends HttpServlet {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestServlet.class);
+
+    private transient ServletConfig config;
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return config;
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        logger.info("send txt to client");
+        String response = ProductService.getProducts(10).toString();
+        resp.setContentType("text/html");
+        resp.getWriter().printf("<h1>Hello, buddy! List of products that you requested: %s </h1>", response);
+        resp.getWriter().close();
+        //resp.getWriter().println(ProductService.getProducts(5));
+
+//        PrintWriter out = resp.getWriter();
+//        resp.setContentType("text/html");
+//        out.println("<html><body><h1>" + infoParam + "</h1></body></html>");
+//        out.close();
+    }
+}
+
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        logger.info("New Get request with includes");
+//
+//        getServletContext().getRequestDispatcher("/header.html").include(req, resp);
+//        resp.getWriter().println("<p>Response body from servlet</p>");
+//        getServletContext().getRequestDispatcher("/footer.html").include(req, resp);
+//    }
+//}
